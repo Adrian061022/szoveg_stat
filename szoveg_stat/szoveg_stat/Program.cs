26558a7b9feda2f6f017dca71564d9faca053f31
Adrian061022 <126463025+Adrian061022@users.noreply.github.com>
2024-03-20 15:05:57 +0100

    elso

---
+++ szoveg_stat/szoveg_stat/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szoveg_stat
+{
+    class Program
+    {
+
+        static int Megszamolas(string a) {
+
+            int db = 1;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == ' ')
+                {
+                    db++;
+                }
+            }
+            return db;
+        
+        }
+
+        static void Kiir(int a) {
+
+            Console.WriteLine($"A szöveg {a} szóbol áll");
+        
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy mondatot");
+            string mondat = Console.ReadLine();
+
+            int megszamolas = Megszamolas(mondat);
+            Kiir(megszamolas);
+
+            Console.WriteLine("nyomj egy billentyűt");
+            Console.ReadKey(true);
+            
+        }
+    }
+}
